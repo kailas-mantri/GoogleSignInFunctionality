01480fd873982e2b9ad7ef5880335a6755c3944f
Kailas <kailasmantri@gmail.com>
2023-05-26 14:42:17 +0530

    complete Project

--- Food18/app/src/main/java/com/scrimatec/food18/models/Urls.java
+++
@@ -1,5 +0,0 @@
-package com.scrimatec.food18.models;
-
-public class Urls {
-    public static final String BASE_URL="https://scrimatec.com/";
-}
--- HelpingHandsPartone/app/src/main/java/com/futuregenerations/helpinghands/GenericFileProvider.java
+++
@@ -1,8 +0,0 @@
-package com.futuregenerations.helpinghands;
-
-import androidx.core.content.FileProvider;
-
-import java.io.File;
-
-public class GenericFileProvider extends FileProvider {
-}
